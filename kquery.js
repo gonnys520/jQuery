22d1cf06863ab38661120ff74bdccfb2aa5024aa
Gonnys520 <gonnys520@gmail.com>
2018-10-10 18:24:10 +0900

    jquery studying

---
+++ kquery.js
@@ -0,0 +1,9 @@
+(function () {
+    console.log("kquery..............?");
+    window.zzz = function () {
+        console.log("zzz..난 이미 자고있다..다만 아직 깨닫지 못해쓸뿐...ㅋㄷ");
+    }
+    window.$ = function (str) {
+        return document.querySelector(str);
+    }
+})();
